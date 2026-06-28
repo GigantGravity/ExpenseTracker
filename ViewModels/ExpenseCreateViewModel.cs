0b3870f34c12f63ae816217d37b064073a4de812
GigantGravity <edwardkenway6@yahoo.com>
2026-06-28 16:39:18 +0200

    feat(expenses): improve expense UI

---
+++ ViewModels/ExpenseCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ExpenseTracker.ViewModels;
+
+public class ExpenseCreateViewModel
+{
+    public Ausgabe Ausgabe { get; set; } = new()
+    {
+        Datum = DateTime.Today
+    };
+
+    public List<SelectListItem> Kategorien { get; set; } = new();
+
+    public List<SelectListItem> Zahlungsarten { get; set; } = new();
+}
