0303d9bebbfbf568836bbf511910bc69f2a7112c
GigantGravity <edwardkenway6@yahoo.com>
2026-06-28 14:12:40 +0200

    feat(categories): display category list

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ExpenseTrackerContext _context;
+
+    public CategoryController(ExpenseTrackerContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var kategorien = await _context.Kategorien
+            .OrderBy(k => k.Name)
+            .ToListAsync();
+
+        return View(kategorien);
+    }
+}
